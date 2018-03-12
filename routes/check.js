19d830987a05601ed659f6b5b6bbc3afa6d6df88
Hey <hey20081234@gmail.com>
2018-03-12 23:13:25 +0800

    implemented check.js web-status-checker

---
+++ routes/check.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+
+const request = require('superagent');
+
+/* GET users listing. */
+router.get('/*', function (req, res, next) {
+    var url = req.param(0);
+
+    if (url){
+        request
+            .get(url)
+            .end((err, result) => {
+                if (err) {
+                    res.json({
+                        isSuccessful: false,
+                        meta: {
+                            isError: true,
+                            err: err
+                        },
+                    });
+                } else {
+                    var isOk = result.ok;
+                    if (isOk) {
+                        res.json({
+                            isSuccessful: true,
+                            meta: {
+                                isError: false,
+                            },
+                        });
+                    } else {
+                        res.json({
+                            isSuccessful: false,
+                            meta: {
+                                isError: false,
+                                result: result
+                            },
+                        });
+                    }
+
+                }
+
+                // Calling the end function will send the request
+            });
+    }
+});
+
+module.exports = router;
