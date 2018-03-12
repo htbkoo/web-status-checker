fc2044ba0e28ef0cd52fd155c2f42be41827268d
Hey <hey20081234@gmail.com>
2018-03-13 00:05:11 +0800

    implemented the react ui part web-status-checker

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,15 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import 'whatwg-fetch';
+
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+const COLOUR = Object.freeze({
+    GREEN: "green-bg",
+    RED: "red-bg",
+    ORANGE: "orange-bg",
+});
 
-      </div>
-    );
-  }
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            bgColor: ""
+        };
+
+        this.interval = 2000;
+        this.hostname = "http://localhost:3000/check";
+        this.pageToCheckUrl = "https://www.npmjs.com/package/cors";
+
+        this.url = `${this.hostname}/${this.pageToCheckUrl}`;
+
+        this.checkUrl = this.checkUrl.bind(this);
+    }
+
+    componentDidMount() {
+        setInterval(this.checkUrl, this.interval);
+    }
+
+    checkUrl() {
+        fetch(this.url)
+            .then(value => value.json())
+            .then(json => json.isSuccessful ? COLOUR.GREEN : COLOUR.RED)
+            .then(bgColor => this.setState({bgColor}))
+            .catch(reason => {
+                console.log(reason);
+                this.setState({bgColor: COLOUR.ORANGE});
+            });
+    }
+
+    render() {
+        let classes = ["App", this.state.bgColor];
+
+        return (
+            <div className={classes.join(" ")}>
+
+            </div>
+        );
+    }
 }
 
 export default App;
