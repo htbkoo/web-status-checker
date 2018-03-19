e21571e3307dd6dbc3f75d3e3efc2af56a718e2c
htbkoo <h1017760@connect.hku.hk>
2018-03-19 20:04:09 +0800

    exposed views in the ui/build_fallback folder as well (as a stale fallback ui version) in order to increase build speed web-status-checker

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'ui/build')));
+app.use(express.static(path.join(__dirname, 'ui/build_fallback')));
 
 app.use('/', index);
 app.use('/check', check);
